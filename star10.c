e906fa9e747339547ae84c13d6e3a8a0a857a111
Shammi Akter <32926144+shammiakter@users.noreply.github.com>
2017-12-11 03:32:47 +0600

    Add files via upload

---
+++ star10.c
@@ -0,0 +1,31 @@
+//triangle
+#include<stdio.h>
+int main()
+{
+    int i, j, r;
+    printf("Enter number of rows : ");
+    scanf("%d", &r);
+
+    for(i=1; i<=r; i++)
+    {
+        for(j=i; j<r; j++)
+        {
+            printf(" ");
+        }
+
+        for(j=1; j<=(2*i-1); j++)
+        {
+
+            if(i==r || j==1 || j==(2*i-1))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
